fb381493aa5416d73c48c1ff5f5785683d969897
Lienhard Laurent <laurent.lienhard@polehabitatstrasbourg.fr>
2020-10-06 18:43:49 +0200

    Add InvokeBuild + Clean task

---
+++ Sources/Resources/HIATCompleteModuleTemplate/BuildTasks/template.02_Clean.Task.ps1
@@ -0,0 +1,12 @@
+﻿task Clean {
+    Write-Verbose ('[{0:O}] [CLEAN][START]' -f (get-date))
+    Write-Verbose ('[{0:O}] [CLEAN] test if output build directory already exist...' -f (get-date))
+    if (Test-Path -Path $env:BHBuildOutput) {
+        Write-Verbose ('[{0:O}] [CLEAN] Pre-build module find => Suppress' -f (get-date))
+        Remove-Item -Path $env:BHBuildOutput -force -recurse -confirm:$false
+    }
+    else {
+        Write-Verbose ('[{0:O}] [CLEAN] Nothing to do...' -f (get-date))
+    }
+    Write-Verbose ('[{0:O}] [CLEAN][END]' -f (get-date))
+}
--- Sources/Resources/HIATCompleteModuleTemplate/_requirements.psd1
+++ Sources/Resources/HIATCompleteModuleTemplate/_requirements.psd1
@@ -7,4 +7,10 @@
     'BuildHelpers'    = @{
         Version = '2.0.15'
     }
+    'PSake' = @{
+        Version = '4.9.0'
+    }
+    'InvokeBuild' = @{
+        Version = '5.6.2'
+    }
 }
