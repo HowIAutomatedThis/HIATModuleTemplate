3db9de74d7204b6e821945efae3f6a1ec8eea5a8
HowIAutomatedThis <68143240+HowIAutomatedThis@users.noreply.github.com>
2020-10-11 17:53:04 +0200

    Update template.04_GenerateMarkdown.Task.ps1

--- Sources/Resources/HIATCompleteModuleTemplate/BuildTasks/template.04_GenerateMarkdown.Task.ps1
+++ Sources/Resources/HIATCompleteModuleTemplate/BuildTasks/template.04_GenerateMarkdown.Task.ps1
@@ -1,15 +1,15 @@
-﻿task GenerateMarkdown {
-    Write-Verbose "[GENERATEMARKDOWN][START]"
+task GenerateMarkdown {
+    Write-Verbose ('[{0:O}] [GENERATEMARKDOWN][START]' -f (get-date))
     $OutputPath = Join-Path $env:BHBuildOutput -ChildPath $env:BHProjectName
     $ModuleBuildPsd1 = Join-Path $OutputPath -ChildPath "$($env:BHProjectName).psd1"
 
     if (Test-Path -Path $ModuleBuildPsd1) {
-
+        Write-Verbose ('[{0:O}] [GENERATEMARKDOWN] Importing module {1}' -f (get-date), $ModuleBuildPsd1)
         $module = Import-Module -FullyQualifiedName $ModuleBuildPsd1 -Force -PassThru
 
         try {
             if ($module.ExportedFunctions.Count -eq 0) {
-                Write-Verbose "[GENERATEMARKDOWN] No functions have been exported for this module. Skipping Markdown generation..."
+                Write-Verbose ('[{0:O}] [GENERATEMARKDOWN] No functions have been exported for this module. Skipping Markdown generation...' -f (get-date))
                 return
             }
 
@@ -18,20 +18,21 @@
                 ErrorAction           = 'SilentlyContinue'
                 Locale                = 'en-US'
                 Module                = $env:BHProjectName
-                OutputFolder          = "$Script:DocsPath\en-US"
+                OutputFolder          = "$OutputPath\Docs\en-US"
                 WithModulePage        = $true
             }
 
             # ErrorAction is set to SilentlyContinue so this
             # command will not overwrite an existing Markdown file.
-            Write-Verbose "[GENERATEMARKDOWN] Creating new Markdown help for $($env:BHProjectName)..."
+            Write-Verbose ('[{0:O}] [GENERATEMARKDOWN] Creating new Markdown help for {1}' -f (get-date), $env:BHProjectName)
             $null = New-MarkdownHelp @params
         }
         finally {
             Remove-Module -Name $env:BHProjectName -Force
         }
 
+    } else {
+        Write-Verbose ('[{0:O}] [GENERATEMARKDOWN] No module to import' -f (get-date))
     }
-
-    Write-Verbose "[GENERATEMARKDOWN][END]"
+    Write-Verbose ('[{0:O}] [GENERATEMARKDOWN][END]' -f (get-date))
 }
